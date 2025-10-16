02679f24e0caae2d54992bf8618a7a55a36cfeba
Vedansh Gupta <vedanshgupta06@gmail.com>
2025-10-16 13:06:01 +0530

    Time: 6 ms (84.77%), Space: 58.2 MB (66.5%) - LeetHub

--- 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.java
+++ 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.java
@@ -2,15 +2,19 @@ class Solution {
     public int findSmallestInteger(int[] nums, int value) {
         int n= nums.length;
         for(int i=0;i<n;i++){
-            nums[i] = nums[i]%value;
+            nums[i] = ((nums[i]%value)+value)%value;
         }
-        int[] freq = new int[value+1];
+        int[] freq = new int[value];
         for(int i=0;i<n;i++){
            if(nums[i]>-1) freq[nums[i]]++;
         }
-        for(int i=0;i<freq.length;i++){
-            if(freq[i]==0)return i;
+        int min=freq[0],idx = 0;
+        for(int i=0;i<value;i++){
+            if(freq[i]<min){
+                min=freq[i];
+                idx=i;
+            }
         }
-    return freq.length;
+    return min*value+idx;
     }
 }
