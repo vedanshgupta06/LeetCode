55ee8b7c6b81db6938d8f664045e8d2ec6eb8ac0
Vedansh Gupta <vedanshgupta06@gmail.com>
2025-10-28 19:18:00 +0530

    Time: 0 ms (100%), Space: 42.1 MB (80.12%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.java
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.java
@@ -1,50 +1,17 @@
 class Solution {
     public int countValidSelections(int[] nums) {
-        int count=0;
-        int n=nums.length;
+        int sum=0;
+        int n=nums.length,count=0;
         for(int i=0;i<n;i++){
-            int[] arr=Arrays.copyOf(nums,nums.length);
-            int dir=1,j=i;
-            if(nums[i]!=0)continue;
-            while(j>=0&&j<n){
-                if(arr[j]==0){
-                    if(dir==1)j++;
-                    else j--;
-                }else if(arr[j]>0){
-                    arr[j]--;
-                    if(dir==1)dir=0;
-                    else dir=1;
-                    if(dir==1)j++;
-                    else j--;
-                }
-            }
-            int sum=0;
-            for(int k=0;k<arr.length;k++){
-                sum+=arr[k];
-            }
-            if(sum==0)count++;
+            sum+=nums[i];
         }
+        int leftSum=0;
         for(int i=0;i<n;i++){
-            int[] arr=Arrays.copyOf(nums,nums.length);
-            int dir=0,j=i;
-            if(nums[i]!=0)continue;
-            while(j>=0&&j<n){
-                if(arr[j]==0){
-                    if(dir==1)j++;
-                    else j--;
-                }else if(arr[j]>0){
-                    arr[j]--;
-                    if(dir==1)dir=0;
-                    else dir=1;
-                    if(dir==1)j++;
-                    else j--;
-                }
+            leftSum+=nums[i];
+            if(nums[i]==0){
+                if(sum-leftSum==leftSum)count+=2;
+                else if(Math.abs((sum-leftSum)-leftSum)==1)count+=1;
             }
-            int sum=0;
-            for(int k=0;k<arr.length;k++){
-                sum+=arr[k];
-            }
-            if(sum==0)count++;
         }
         return count;
     }
